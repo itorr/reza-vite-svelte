b309175ec8923937dd57cc1c9172a70836ec8801
itorr-i7 <592668024@qq.com>
2024-03-01 04:17:59 +0800

    port fix

--- src/functions/config.mjs
+++ src/functions/config.mjs
@@ -1,3 +1,3 @@
-export const BASE_MUSIC_URL = 'https://n.magiconch.com:8087/music/';
+export const BASE_MUSIC_URL = 'https://n.magiconch.com:8088/music/';
 
 export const BASE_MUSIC_ALBUMS_URL = `${BASE_MUSIC_URL}albums/`;
