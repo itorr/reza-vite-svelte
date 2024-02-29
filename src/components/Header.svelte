cd0261d805dfbedc6ef0e8263e4e9def199186ea
itorr-i7 <592668024@qq.com>
2024-02-29 22:43:03 +0800

    can-i-keep-unused-css-selector

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,28 @@
 <script>
+import active from "svelte-spa-router/active";
 
 </script>
 
-<header>
-	<h1>Albums</h1>
+<header class="head-bar">
+	<h1>Reza!</h1>
 	<nav>
-		<a href="#/">首页</a>
-		<a href="#/albums">专辑列表</a>
+		<a href="#/" use:active>首页</a>
+		<a href="#/albums" use:active>专辑列表</a>
 	</nav>
 </header>
+
+<style lang="less">
+.head-bar{
+	nav{
+		a{
+			margin-right: 10px;
+			
+			// https://stackoverflow.com/questions/68499862/can-i-keep-unused-css-selector
+			:global(&.active){
+				color:red;
+				font-weight: bold;
+			}
+		}
+	}
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@
 import Counter from "./views/Counter.svelte";
 import Albums from "./views/Albums.svelte";
 
-
+// https://github.com/ItalyPaleAle/svelte-spa-router
 export const routes = {
     '/': Counter,
     '/albums': Albums,
